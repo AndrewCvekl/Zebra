ad4f0a3ce57e08bcb651238e84b3e21a35c0997a
Andrew <andrewcvekl81@gmail.com>
2016-07-12 10:50:24 -0400

    initial commit

---
+++ Zebra/SummaryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Zebra
+//
+//  Created by Ales Cvekl on 7/8/16.
+//  Copyright © 2016 Andrew Cvekl. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+    
+    
+}
+
